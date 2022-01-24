9dab0f0832c5aa7c5f3f834210531f3a13cdbcdb
pomeranian91 <forking91@gmail.com>
2022-01-25 03:11:21 +0900

    Add: 스타일,  기능구현 추가

--- src/components/CalculateSecond/CalculateRateBox.js
+++ src/components/CalculateSecond/CalculateRateBox.js
@@ -1,25 +1,30 @@
-import React from 'react';
+import React from "react";
+import "./CalculateRateBox.css";
 
-const CalculateRateBox = ({test}) => {
-    const rateList = ['CAD', 'KRW', 'HKD', 'JPY', 'CNY'];
+const CalculateRateBox = () => {
+  const rateList = ["CAD", "KRW", "HKD", "JPY", "CNY"];
+  const test = "CAD";
+  const money = "20,000.00";
 
-    return (
-        <div className="container">
-            <ul className="tabs">
-                {
-                    rateList.map((rate, index) => {
-                        return (
-                        <li className={`tab ${rate === test && 'actice'}`} key={index}>{rate}</li>
-                        )
-                    })
-                }
-            </ul>
-            <div className="tabsInfo">
-                <p className="countryRate">2,000.00</p>
-                <p className="rateLiveDate">기준일: <br /> 2022-Jan-01</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="CalculateRateBox">
+      <ul className="tabs">
+        {rateList.map((rate, index) => {
+          return (
+            <li className={`tab ${rate === test && "active"}`} key={index}>
+              {rate}
+            </li>
+          );
+        })}
+      </ul>
+      <div className="tabsInfo">
+        <p className="countryRate">{`${test} : ${money}`} </p>
+        <p className="rateLiveDate">
+          기준일: <br /> 2022-Jan-01
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default CalculateRateBox;
--- src/components/CalculateSecond/CalculateSecondForm.js
+++ src/components/CalculateSecond/CalculateSecondForm.js
@@ -1,25 +1,53 @@
-import React, { useState } from 'react';
-import CalculateRateBox from './CalculateRateBox.js';
-import './CalculateSecondForm.css';
+import React, { useState } from "react";
+import CalculateRateBox from "./CalculateRateBox.js";
+import "./CalculateSecondForm.css";
+import client from "../../pages/Main/lib/api/client";
 
 const CalculateSecondForm = () => {
-    const [countryRates, setCountryRates] = useState(['USD', 'KRW', 'HKD', 'JPY', 'CNY'])
+  const [countryRates, setCountryRates] = useState(["USD", "CAD", "KRW", "HKD", "JPY", "CNY"]);
+  const [currentExchangedMoney, setCurrentExchangedMoney] = useState(0);
+  const [currentSelectedCountry, setCurrentSelectedCountry] = useState("USD");
 
-    return (
-        <div className="container">
-            <div className="controllerHeader">
-                <input type="number" />
-                <select>
-                    {
-                        countryRates.map((countryRate, index) => {
-                        return <option key={index} value={countryRate}>{countryRate}</option>
-                        })
-                    }
-                </select>
-            </div>
-            <CalculateRateBox />
-        </div>
-    )
-}
+  const getExchangedMoney = (currentInputedMoney) => {
+    return client.get("http://api.currencylayer.com/live", {
+      params: {
+        access_key: process.env.REACT_APP_API_KEY,
+        source: currentSelectedCountry,
+        amount: currentInputedMoney,
+      },
+    });
+  };
+
+  const checkInputedController = async (event) => {
+    const currentTargetedController = event.target.nodeName;
+    if (currentTargetedController === "INPUT") {
+      const currentInputedMoney = event.target.value;
+      const response = await getExchangedMoney(currentInputedMoney);
+      const {
+        data: { quotes, timestamp },
+      } = response;
+      console.log(quotes, timestamp);
+    } else if (currentTargetedController === "SELECT") {
+      const currentSelectedCountry = event.target.options[event.target.selectedIndex].value;
+    }
+  };
+  return (
+    <div className="calculateSecondForm">
+      <div className="controllerHeader" onChange={checkInputedController}>
+        <input className="rateInput" type="number" />
+        <select className="rateSelected">
+          {countryRates.map((countryRate, index) => {
+            return (
+              <option key={index} value={countryRate}>
+                {countryRate}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <CalculateRateBox />
+    </div>
+  );
+};
 
 export default CalculateSecondForm;
