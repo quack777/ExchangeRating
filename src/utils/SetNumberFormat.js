2f007669fe1199ca931873151fa8552599a2d0a9
pomeranian91 <forking91@gmail.com>
2022-01-25 17:52:22 +0900

    Modify: 인풋콤마 설정

--- src/utils/SetNumberFormat.js
+++ src/utils/SetNumberFormat.js
@@ -1,5 +1,16 @@
 function SetNumberFormat(cost) {
-    let tmpCost = cost.toFixed(2);
-    return tmpCost.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  let tmpCost = cost.toFixed(2);
+  return tmpCost.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+export const inputPriceFormat = (str) => {
+  const comma = (str) => {
+    str = String(str);
+    return str.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, "$1,");
+  };
+  const uncomma = (str) => {
+    str = String(str);
+    return str.replace(/[^\d]+/g, "");
+  };
+  return comma(uncomma(str));
+};
 export default SetNumberFormat;
