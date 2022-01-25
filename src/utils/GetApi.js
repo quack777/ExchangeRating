edd2f0499c0e74aa0ec9e3762e378068610a5132
pomeranian91 <forking91@gmail.com>
2022-01-25 19:21:15 +0900

    Modify: 오류수정

--- src/utils/GetApi.js
+++ src/utils/GetApi.js
@@ -1,13 +1,13 @@
-import client from "../lib/api/client";
+import client from "../pages/Main/lib/api/client";
 
-const GetApi = (currentInputedMoney,currentSelectedCountry) => {
-        return client.get("http://api.currencylayer.com/live", {
-            params: {
-                access_key: process.env.REACT_APP_API_KEY,
-                source: currentSelectedCountry,
-                amount: currentInputedMoney,
-            },
-        });
-    };
+const GetApi = (currentInputedMoney, currentSelectedCountry) => {
+  return client.get("http://api.currencylayer.com/live", {
+    params: {
+      access_key: process.env.REACT_APP_API_KEY,
+      source: currentSelectedCountry,
+      amount: currentInputedMoney,
+    },
+  });
+};
 
 export default GetApi;
