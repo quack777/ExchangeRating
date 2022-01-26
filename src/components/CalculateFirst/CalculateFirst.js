bf4cfec1eb36e9789edbe503f43920d43bd8d32b
Mcdonalds Trump <whdmschd2@gmail.com>
2022-01-26 17:31:44 +0900

    Refactor: first calculate

--- src/components/CalculateFirst/CalculateFirst.js
+++ src/components/CalculateFirst/CalculateFirst.js
@@ -8,6 +8,7 @@ const CalculateFirst = () => {
     const [userChoice, setUserChoice] = useState('KRW');
     const [sendMoney,setSendMoney] = useState();
     const [isSubmit, setIsSubmit] = useState(false);
+    const [apiSuccess, setApiSuccess] = useState(false);
 
     const handleResultOfFirstCalculator=(e)=>{
         e.preventDefault();
@@ -22,11 +23,23 @@ const CalculateFirst = () => {
         e.target[1].value = ''
     }
 
+    const countrys = [
+        {engName: "KRW", korName: "한국"},
+        {engName: "JPY", korName: "일본"}, 
+        {engName: "PHP", korName: "필리핀"}
+    ];
+
     useEffect(()=>{
         setIsSubmit(false)
-        getApiOfExchangeRate("USD")
-            .then((res)=>setExchangeRate(res.data.quotes))
-            .catch((err)=>console.dir(err))
+        getApiOfExchangeRate("USD", countrys.map(e => e.engName).join(","))
+            .then((res) => {
+                if (res.data.success === true) {
+                    console.log(res.data.quotes)
+                    setExchangeRate(res.data.quotes);
+                    setApiSuccess(true);
+                }
+            })
+            .catch((err) => console.dir(err))
     },[userChoice]) 
 
     return(
@@ -35,23 +48,24 @@ const CalculateFirst = () => {
             <p className = 'first-title'>환율 계산</p>
             <p>송금국가 : 미국(USD)</p>
             <div>수취국가 :
-              <select onChange={(event)=>setUserChoice(event.target.value)}>
-                  <option value="KRW">한국(KRW)</option>
-                  <option value="JPY">일본(JPY)</option>
-                  <option value ="PHP">필리핀(PHP)</option>
-              </select>
-              </div>
+                <select onChange={(event) => setUserChoice(event.target.value)}>
+                    {countrys.map((data, i) => {
+                        return(
+                            <option value={data.engName}>{data.korName}({data.engName})</option>
+                        )
+                    })}
+                </select>
+            </div>
                 <p>
-                    환율: {exchangeRate && SetNumberFormat(exchangeRate[`USD${userChoice}`])} {userChoice}/USD
+                    환율: {apiSuccess && SetNumberFormat(exchangeRate[`USD${userChoice}`])} {userChoice}/USD
                 </p>
                 <p>
                     송금액: <input/> USD
                 </p>
                 <button className='first-btn' type='submit'>Submit</button>
-            </form>
-                {isSubmit? <div className='first-res'>수취금액은 {exchangeRate && SetNumberFormat(sendMoney*exchangeRate[`USD${userChoice}`])} {userChoice}입니다</div> : ''}
-        </div>
-  )
-};
+        </form>
+        {isSubmit? <div className='first-res'>수취금액은 {apiSuccess && SetNumberFormat(sendMoney*exchangeRate[`USD${userChoice}`])} {userChoice}입니다</div> : ''}
+    </div>
+)};
 
 export default CalculateFirst;
